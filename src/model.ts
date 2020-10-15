f729cd57f8521b1246618c12a5914a1a1a74f418
lnilya <lnilya>
2020-10-14 18:14:59 -0700

    Some Ignores

--- src/model.ts
+++ src/model.ts
@@ -1,13 +1,13 @@
 
-//An example Atom which stores a random number
 import {connectedAtom, connectedSelector, updateConnectedValue} from "./ConnectedStore";
 
+//An example Atom which stores a random number
 export const randomNumber = connectedAtom<number>({
 	key:'random_number',
 	default: 0
 });
 
-//An example selector which makes the randomNumber Atom negative, and can also be set
+//An example selector which makes the randomNumber Atom negative, and can also be set directly
 export const negativeRandomNumber = connectedSelector<number>({
 	key: 'neg_random_number',
 	get: ({get}) => get(randomNumber) * -1,
